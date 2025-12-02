220e3f7671680154b8c677c6b1a4b9c33f57ea6f
Thomas Swarbrick <swartom@localhost-live.a09.lan>
2025-12-02 15:46:15 +0000

    Passthroguh

--- scale_free/__main__.py
+++ scale_free/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-threads = [4,8,16,32,64]
+threads = [1,2,4,8,16,32,64]
 repeats = 1
 graph= 1_000_000_000
 if __name__ == '__main__':
@@ -50,6 +50,6 @@ if __name__ == '__main__':
         x = data[:,0]
         y = data[:,1]
         plt.plot(x,y,'g',marker='o')
-    plt.xlabel("Nodes")
+    plt.xlabel("Threads")
     plt.ylabel("Rate (Million particles/second)")
     plt.savefig('output.pdf')

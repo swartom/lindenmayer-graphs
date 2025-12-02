081681b4fa87e1215dfdea4d3631ace14452e48b
Thomas Swarbrick <swartom@localhost-live.a09.lan>
2025-12-02 10:58:31 +0000

    Passthroguh

--- scale_free/__main__.py
+++ scale_free/__main__.py
@@ -2,8 +2,8 @@
 
 
 threads = [1,2,4,8,16,32,64]
-repeats = 1
-graph= 250_000
+repeats = 10
+graph= 25_000
 if __name__ == '__main__':
     print("Building Figures for The Scale-free graph")
     import os
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         datapoint =list()
         for i in threads:
             time.sleep(10)
-            cmd = f"./sf {graph*i} {4} {i} {0.50}"
+            cmd = f"./sf {graph*i} {40} {i} {0.50}"
             os.system(cmd)
             result = os.popen(cmd).read()
             total = float(result.split('s')[0])

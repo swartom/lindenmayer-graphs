0a6665ceb900cac49c8791bd2014012da3cb1939
Thomas Swarbrick <swartom@localhost-live.a09.lan>
2025-12-10 11:13:44 +0000

    passthrough

--- scale_free/__main__.py
+++ scale_free/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 threads = [1,2,4,8,16,32]  # 1,2,4,
 repeats = 1
-graph= 1_000_000_000# 1_000_000#_000
+graph= 100_000_000#_000# 1_000_000#_000
 if __name__ == '__main__':
     print("Building Figures for The Scale-free graph")
     import os
@@ -12,20 +12,23 @@ if __name__ == '__main__':
 
     edges = 3
     for a in range(repeats):
-        print(a)
+
         datapoint =list()
         for i in threads:
             cmd = f"./sf {graph} {edges} {i} {0.50}"
             os.system(cmd)
             result = os.popen(cmd).read()
+            print(result)
             total = float(result.split('s')[0])
-            datapoint.append((i,(total)))
+            print(total,result.split('s')[0])
+            datapoint.append((i,total))
             print(f"{i},{total}")
 
         data = np.array(datapoint)
         x = data[:,0]
         y = data[:,1]
         z = y/x
+        y = y[0]/y
         plt.figure(1)
         plt.plot(x,z,'b',marker='x')
         plt.xlabel("Threads")
@@ -51,6 +54,7 @@ if __name__ == '__main__':
         data = np.array(datapoint)
         x = data[:,0]
         y = data[:,1]
+
         plt.plot(x,y,'g',marker='o')
     plt.xlabel("Threads")
     plt.ylabel("Seconds")

f3d8df5cef6c72e7e93d81edc1b7d2504be6ea79
Thomas Swarbrick <swartom@localhost-live.a09.lan>
2025-12-10 15:37:36 +0000

    Passthrough

--- discrete/__main__.py
+++ discrete/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 threads = [2,4,8,16,32]
-graphs = [  i for i in range(1,6) ]
+graphs = [  i for i in range(3,6) ]
 repeats = 1
 if __name__ == '__main__':
     print("Building Figures for The Discrete graph")

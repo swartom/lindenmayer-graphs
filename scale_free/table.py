75f8e3a727985b7233344e476929102a9a4aaf87
Thomas Swarbrick <swartom@localhost-live.a09.lan>
2025-12-11 15:43:59 +0000

    passthroguh

--- scale_free/table.py
+++ scale_free/table.py
@@ -10,10 +10,10 @@ os.system(cmd)
 import numpy as np
 keys = []
 threads = [32]
-graphs = range(2,8)
+graphs = range(1,7)
 options= [ 10**(i) for i in graphs]
 headers = [ f'10^{i}' for i in graphs]
-edges=  [2**(i) for i in range(8)]
+edges=  [2**(i+4) for i in range(4)]
 for a in edges:
     print(f"Count: {a}")
     for edge in threads:
@@ -23,35 +23,10 @@ for a in edges:
             cmd = f"./sf {i} {edge} {a} {0.50}"
             os.system(cmd)
             result = os.popen(cmd).read()
-
-
-            result =  result.split("\n")
-            from scipy import stats
-
-            import statistics
-            import pandas as pd
-
-            array = [float(r) for r in result if r != "" ]
-            import pandas as pd
-            import numpy as np
-            from scipy import stats
-
-            df = pd.DataFrame(array)
-
-            df = df[(np.abs(stats.zscore(df)) < 1).all(axis=1)]
-            array = [float(f) for f in df.to_numpy()]
-            print(array)
-
-
-            # df = pd.DataFrame(data)
-            # df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
-
-
-
-            geo = statistics.mean(array)
-            sd = statistics.stdev(array)
-            datapoint.append(f'${geo*1000:.1f}ms\\pm {(sd/geo)*100:.1f}\\% ({len(array)})$')
-        key = f'{edge}-{a}'
+            total = float(result.split('s')[0])
+            sd = float(result.split('s')[1])
+            datapoint.append(f'${total*1000:.2f}ms\\pm {(sd/total)*100:.2f}\\%$')
+        key = f'$d={a}$'
         data[key] = datapoint
         keys.append(key)
 
@@ -59,11 +34,8 @@ textabular = f"r||{'r'*len(headers)}"
 texheader = " & $" + "$ & $".join(headers) + "$\\\\"
 texdata = "\\hline\n"
 for label in keys:
-    if label.split('-')[1]=='1':
-        texdata += '\\hline '
     texdata += f"{label} & {' & '.join(map(str,data[label]))} \\\\\n"
-    if label.split('-')[1]=='1':
-        texdata += '\\hline '
+
 
 print("\\begin{tabular}{"+textabular+"}")
 print(texheader)

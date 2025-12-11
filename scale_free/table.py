abf965da49ba37573f6a7b5bdb836208ca0959d6
Thomas Swarbrick <swartom@localhost-live.a09.lan>
2025-12-11 15:45:03 +0000

    Passthrough

--- scale_free/table.py
+++ scale_free/table.py
@@ -9,11 +9,11 @@ cmd = "gcc -lcblas -lgsl -O3 -lm -pthread ./scale_free/scale_free.c -o sf"
 os.system(cmd)
 import numpy as np
 keys = []
-threads = [32]
+threads = [64]
 graphs = range(1,7)
 options= [ 10**(i) for i in graphs]
 headers = [ f'10^{i}' for i in graphs]
-edges=  [2**(i+4) for i in range(4)]
+edges=  [2**(i) for i in range(8)]
 for a in edges:
     print(f"Count: {a}")
     for edge in threads:
